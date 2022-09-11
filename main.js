5e75b1fc85b1c27dd985ddc26435fd003a3bf966
Alexandra <alexandraneff@live.com>
2022-09-10 21:27:45 -0500

    commit tree 7f3fd4d8b64b9adb9660153622728aa9b39fbcf2

--- main.js
+++ main.js
@@ -2,9 +2,48 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+// Your JavaScript code goes here! 
 
+//To hide the error message 
+modal = document.getElementById('modal')
+modal.classList.add('hidden')
+//To grab where the empty hearts are
+const like = document.getElementsByClassName('like-glyph')
+//My like event listener 
+console.log(like)
+const likeArray = Array.from(like)
+console.log(likeArray)
+likeArray.forEach(e => {
+  e.addEventListener('click', handleLike)
+})
 
+function handleLike(e) {
+mimicServerCall()
+.then(function () {
+  let notLiked = e.target.innerText === EMPTY_HEART
+  if (notLiked) {
+e.target.innerText = FULL_HEART
+e.target.classList.add('activated-heart')
+console.log(e.target.classList)
+  } else {
+e.target.innerText = EMPTY_HEART
+e.target.classList.remove('activated-heart')
+console.log(e.target.classList)
+  }
+  //innerText will need to be updated to the "full-heart"
+  //will also need to add the class name of activated-heart
+  //will need and if else statment for if heart is full or empty 
+})
+
+.catch(function (error) {
+  modal.classList.remove('hidden')
+  modal.innerText = error
+  //setTimeout function
+  setTimeout(function() {
+    modal.classList.add('hidden')
+  }, 3000) 
+})
+}
 
 
 //------------------------------------------------------------------------------
